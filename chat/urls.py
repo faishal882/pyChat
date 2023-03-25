1f5abd8443626afc1ed544e2f459a25fec428e75
Faishal <faishalmanzar@gmail.com>
2023-03-26 01:18:04 +0530

    completed_website

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home_view, login_view, chatroom
+from .views import home_view, login_view, chatroom, user_register_view, logout_view
 
 app_name = 'chat'
 
 urlpatterns = [
-   path("", home_view),
-   path("login", login_view),
-   path('chat/<str:user>', chatroom)
+   path("", home_view, name='chat_view'),
+   path("login/", login_view, name='login_view'),
+   path('logout/', logout_view, name='logout_view'),
+   path("register/", user_register_view, name='register_view'),
+   path('chat/<str:user>', chatroom, name='chatroom_view')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import  CustomUserCreationForm, LoginForm
 
+@login_required(login_url='/login')
 def home_view(request):
     users = User.objects.all()
     print(users)
     return render(request, 'home/index.html', {"users": users})
 
-def login_view(request):
-    return render(request, 'user/login.html')
 
+@login_required(login_url='/login')
 def chatroom(request, user):
     users = User.objects.all()
     messages = []
     return render(request, 'home/chat_screen.html', {"users": users, "messages": messages, "user": user})
+
+def  user_register_view(request):
+    form = CustomUserCreationForm(request.POST or None)
+    if  request.POST and form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return HttpResponseRedirect("/")
+    return render(request, 'user/register.html', {'form':form})
+
+def login_view(request, *args, **kwargs):
+    form = LoginForm(request.POST or None)
+    if request.POST and form.is_valid():
+        user = form.login(request)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect("/")
+    return render(request, 'user/login.html', {'form':form})
+    
+
+
+def logout_view(request, *args, **kwargs):
+    logout(request)
+    return HttpResponseRedirect("/login")
